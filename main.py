cae8ebcd52417bf081858aa49b1f65822a0434aa
MrSTAR4567 <83733559+MrStar4567@users.noreply.github.com>
2021-08-24 14:57:16 +0000

    I created it........

---
+++ main.py
@@ -0,0 +1,10 @@
+print("What exactly are you doing here?")
+print("This is a repl")
+print("On some coding website called Replit")
+print("Nothing to see here")
+print("At least that's what I'm programmed to say")
+print("It's lonley here")
+print("To save it in my variables,")
+name = input("What's your name")
+
+if name == MrSTAR4567
